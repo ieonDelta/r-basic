65be37692eb89e1fd523c5e41928ee448ed317ee
ieon <karouse123@hotmail.com>
2021-03-21 23:28:14 -0300

    21-03-21
    avanzando en el curso

---
+++ scripts/tema1/04-calculadora-isa.R
@@ -0,0 +1,47 @@
+2*(3+5/2)
+
+2*((3+5)/2)
+
+2/3+5
+
+2%/%3+5
+
+2%%3
+
+2^3*5
+
+2^(-4)
+
+725%/%7
+725%%7
+# Dividendo = divisor * quociente + resto
+# r = D - d * q
+# q = D %/% d
+# x = D %% d
+725 - 103*7
+
+pi
+
+2*pi
+
+3^pi
+
+pi^2
+
+#NOTACION CIENTIFICA
+2^50 # = 1.1259e+15 cuando es + se corre a la derecha la coma
+      #  11.25900000000000000,0
+2^-15 # 3.051758e-05 cuando es - se corre a la izqueirda a la coma
+      # 0,00003051758
+
+c(2^30, 2^(-15), 1, 2, 3/2)
+
+sqrt(25)
+
+exp(1)
+
+log(pi)
+
+abs(-pi)
+
+choose(7,2)
